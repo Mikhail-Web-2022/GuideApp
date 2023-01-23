6cc018476ce32d76065ca794211920aee5a023fd
“Mikhail-Web-2022” <vinokurov16@gmail.com>
2023-01-23 19:55:17 +0200

    Initial Commit

---
+++ guide-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guide-app
+//
+//  Created by Михаил Винокуров on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
